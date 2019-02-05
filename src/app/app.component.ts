044c836ec3a15950be35b370277be430aafd3f11
Sagar Joshi <sagar.2.joshi@atos.net>
2019-02-05 16:15:52 +0530

    added bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SampleAngular6Project';
+  title = 'SampleAngular6Project Tutorial by Sagar';
 }
